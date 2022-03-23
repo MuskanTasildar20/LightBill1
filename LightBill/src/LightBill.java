2db10461888f0c9e4b041efdf87131ebef274097
admin <admin@Bcalab4-m17>
2022-03-23 10:53:01 +0530

    first commit....

---
+++ LightBill/src/LightBill.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LightBill {
+	int c_id;
+	int ucost;
+	float nofcost;
+	LightBill(int c_id, int ucost, float nofcost){
+		this.c_id=c_id;
+		this.nofcost=nofcost;
+		this.ucost=ucost;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
