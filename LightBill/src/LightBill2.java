6c79062ebd73fb08e8758f16656cdd84f95fa626
admin <admin@Bcalab4-m18>
2022-03-23 11:31:33 +0530

    commit

--- LightBill/src/LightBill2.java
+++ LightBill/src/LightBill2.java
@@ -1,12 +1,12 @@
 import java.util.*;
-public class LightBill {
+public class LightBill2 {
 	int c_id;
 	int ucost;
 	float nofcost;
-	LightBill(int c_id, int ucost, float nofcost){
+	LightBill2(int c_id, int ucost, float nofcost){
 		this.c_id=c_id;
 		this.nofcost=nofcost;
-		this.ucost=ucost; 
+		this.ucost=ucost;
 	}
 
 	public static void main(String[] args) {
